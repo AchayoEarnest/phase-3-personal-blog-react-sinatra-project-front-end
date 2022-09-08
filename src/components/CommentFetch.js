9162ec40319a70e8cd15c1eb4ebe23a9e2ef6baa
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-09-08 12:37:36 +0300

    Added delete button functionality

--- src/components/CommentFetch.js
+++ src/components/CommentFetch.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function CommentFetch() {
-    
+const apiComment = "http://localhost:9292/create_comments"
+
+
+function CommentFetch({ comento, deleteComment }) {
+    const { name, email, reply } = comento;
+    const [read, setRead] = useState(false);
+    const [favourite, seFavourite] = useState(false);
+
+    function handleRead() {
+        setRead(!read);
+    }
+    function handleFavourite() {
+        seFavourite(!favourite);
+    }
+    function handleDelete(e) {
+        e.preventDefault();
+        fetch(`${apiComment}/${comento.id}`, {
+            method: 'DELETE',
+        })
+            .then((res) => res.json())
+            .then((comento) => {
+                deleteComment(comento);
+            });        
+     }
+
+
+
   return (
-    <div>CommentFetch</div>
+      <div>
+          <h3>Name: {name}</h3>
+          <h3>Email: {email}</h3>
+          <p>Comment: { reply }</p>
+          <button onClick={ handleRead }>Mark as { read ? "Unread" : "Read" }</button>
+          
+          <br></br>
+          <button onClick={ handleDelete }>Delete</button>
+          <button onClick={ handleFavourite }>{ " " } Mark as { favourite ? "Not favourite" : "My favourite" }</button>
+
+    </div>
   )
 }
 
