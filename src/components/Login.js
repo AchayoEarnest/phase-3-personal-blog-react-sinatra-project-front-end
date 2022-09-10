95b7347f56d3649cefa99c2d8f888287032236cb
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-09-10 17:09:37 +0300

    added codes to authenticate from login page to main Home page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,57 +2,100 @@ import React, { useEffect, useRef, useState } from 'react'
 import {Button, Form } from 'semantic-ui-react'
 import './Login.css'
 import { Link } from 'react-router-dom';
+import Home from "./Home"
 
 const Login = () => {
     const userRef = useRef();
     const errRef = useRef();
 
 
-    const [user, setuser] = useState('');
+    const [user, setUser] = useState('');
     const [pass, setPass] = useState('');
     const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
 
 
-    useEffect(() => {
-        useRef.current.focus();
-    }, [])
+        // useEffect(() => {
+        //     errMsg('');
+        // }, []) 
 
-    useEffect(() => {
-        setErrMsg('');
-    }, [user, pass])
+        useEffect(() => {
+            setErrMsg('');
+        }, [user, pass])
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user, pass);
+        setUser('');
+        setPass('');
+        setSuccess(true);
+    }
 
 
 
-    return (        
-        <div className='Form_Container'>
-        Login
-        <div class='Form_Inner_Container'>
-            <Form>
-                <Form.Group widths='equal'>
-                    <Form.Input
-                        fluid
-                        id='form-subcomponent-shorthand-input-first-name'
-                        label='Email'
-                        type='email'
-                        placeholder='First name'
-                    />
-                    <Form.Input
-                        fluid
-                        id='form-subcomponent-shorthand-input-last-name'
-                        label='Password'
-                        type='password'
-                        placeholder='Password'
-                    />
-                    </Form.Group>
-                <Button>Submit</Button>
-                <br></br>
-                <Link to='/signup' className='Nav_Links_2'>Signup</Link>
-                </Form>
-        </div>
+    return (    
+        <>
+            {success?(
+                <section>
+                    {/* <h1>You are logged in!</h1>
+                    <br />
+                    <p>
+                        <a href="#">Go to Home</a>
+                    </p> */}
+                    <Home />
+                </section>
+            
+            ) : (
+                <section className='Form_Container'>
+                <p ref={errRef} className={errMsg? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}
+                
+                </p>
+            
+                <div class='Form_Inner_Container'>
+                    <h3 className='Login_Head'>Login</h3>
+                
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group widths='equal'>
+                        <label htmlFor='username'>Username</label>
+                            <Form.Input
+                                fluid
+                                id='username'                            
+                                type='text'
+                                ref={ userRef }
+                                autoComplete="off"
+                                placeholder='First name'
+                                onChange={ (e) => setPass(e.target.value) }
+                                value={user}
+                                required
+                                
+                            />
+                        <label htmlFor='password'>Password</label>
+                            <Form.Input
+                                fluid
+                                id='password'
+                                type='password'
+                                autoComplete="off"
+                                placeholder='Password'
+                                onChange={ (e) => setUser(e.target.value) }
+                                value={pass}
+                                required
+                            />
+                        </Form.Group>
+                        <Button>Sign In</Button>
+                        <br />
+                        Need an Account?
+                        <Link to='/signup' className='Nav_Links_2'>Signup</Link>
+                        </Form>
+                </div>       
+            
+      </section>
+                    
+        )
+
+        }
         
+        </>
         
-  </div>
     
 )
 }
